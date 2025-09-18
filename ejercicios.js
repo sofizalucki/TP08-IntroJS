4173d2f9b0f3b6bdc111bc48b3b3c8f6b5500814
JOAQUÍN IRIBAS <49627045@ort>
2025-09-18 14:30:17 -0300

    pop

--- ejercicios.js
+++ ejercicios.js
@@ -1,6 +1,6 @@
 const resultado = document.getElementById('resultado')
 const frutas = ["pera", "banana", "crotolamo", "naranja", "zandia", "olmo", "melon", "pomelo", "kiwi", "arandano"]
-
+resultado.textContent = "Aca se veran sus resultados"
 function clearCons(){
     console.clear();
     resultado.textContent = ""
@@ -36,4 +36,44 @@ function Ejercicio3(){
     console.log(tres ? "Sip, 10 es === a '10' " : "Nop, 10 no es === a '10' ")
     console.log(`el tipo de dato de 10.6 es ${typeof 10.6}`)
     console.log(true == 1)
+}
+function Ejercicio4() {
+    const ciudad = {
+        nombre: prompt("Ingrese el nombre de su ciudad"),
+        poblacion: prompt("Ingrese la poblacion de su ciudad"),
+        fechaFundacion: prompt("Ingrese la fecha de fundacion de su ciudad"),
+        extension: prompt("Ingrese la extension de su ciudad")
+    };
+
+    resultado.innerHTML = "Estos son los atributos de su ciudad:<br>";
+    for (const atributo in ciudad) {
+        resultado.innerHTML += `${atributo}: ${ciudad[atributo]}<br>`;
+    }
+}
+function  Ejercicio5(){
+    const juntos = document.getElementById('numeros').value;
+    const separados = juntos.split(',')   
+    const dobles = separados.map(n => n * 2);
+    console.log(dobles)
+    resultado.innerHTML = dobles;
+}
+function Ejercisio6A{
+
+}
+function Ejercisio6B{
+    
+}
+
+function Ejercicio7(){
+    const input = document.getElementById('Noms').value;
+    const separados = input.split(',')
+    const elegidos = elegirNombresConA(separados)
+    document.getElementById('resultado').textContent = elegidos.join(', ')
+}
+function Ejercicio8(){
+    const text = document.getElementById('cadtxt').value;
+    const busc = document.getElementById('buscarP').value;
+    const reemp = document.getElementById('reemplazarP').value;
+    const resultado = reemplazarTexto(text, busc, reemp);
+    document.getElementById('resultado').textContent = resultado;
 }
--- utils.js
+++ utils.js
@@ -11,6 +11,15 @@ function CalcularEdad(fechaNum){
     if (mesActual < mesNacimiento || (mesActual === mesNacimiento && diaActual < diaNacimiento)) {
         edad--;
     }
-    return edad;
-  
+    return edad
+}
+
+function elegirNombresConA(nombres) {
+    return nombres.filter(nombre => nombre.trim().charAt(0).toUpperCase() === 'A');
+}
+
+function reemplazarTexto(text, busc, reemp) 
+{
+    const regex = new RegExp(busc, 'gi');
+    return text.replace(regex, reemp);
 }
